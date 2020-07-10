82f6df762e660c7e8144cedab42f230d97968f83
Ibrahim Zbib <abraham.zbib@gmail.com>
2020-07-09 20:55:10 -0400

    pt2 complete now

--- app.js
+++ app.js
@@ -1,13 +1,47 @@
-var friends = ["Sam", "Diana", "Haley", "Adam", "Joe"];
+
+
+(document.body.getElementsByTagName("h1")[0]).remove();
+(document.body.getElementsByClassName("friend")[0]).remove();
+document.addEventListener("DOMContentLoaded", function(){
+      
+    var button = document.createElement("button");
+    var buttonText = document.createTextNode("Sing");
+   
+    button.id = "sqrbtn";
+    button.style.backgroundColor = "pink";
+  
+    button.style.borderColor = "blue";
+    button.style.marginLeft = "auto";
+    button.style.marginRight = "auto";
+    button.style.marginTop = "25px";
+    button.style.marginBottom = "10px";
+    button.style.display = "block";
+    button.appendChild(buttonText);
+    document.body.appendChild(button);
+    button.addEventListener("click", function(){
+                            
+                            var friends = ["Sam", "Diana", "Haley", "Adam", "Joe"];
+                            
 for(var x = 0; x < friends.length; x++){
-    for(var y = 100; y > 0; y--){
-    
-                if(y === 100)
-                 console.log((friends[x]).toUpperCase() + ":");
-                else if(y === 1)
-                     console.log("Now for 1 last time, I want you to know " + friends[x] +" that I am lucky to have you in my life");
+        var div = document.createElement("div");
+        var h3 = document.createElement("h3");
+        div.className = "friend";
+     var friendname = document.createTextNode((friends[x]).toUpperCase() + ":");
+    h3.appendChild(friendname);
+    div.appendChild(h3);
+    for(var y = 99; y > 0; y--){
+       var p = document.createElement("p");
+        var ptext;
+                if(y === 1)
+                     ptext = document.createTextNode("Now for 1 last time, I want you to know " + friends[x] +" that I am lucky to have you in my life");
                 else
-                    console.log(y + " more times, thats how many more times I am going to tell you " + friends[x] +" that I am lucky to have you in my life");
-    
+                   ptext = document.createTextNode(y + " more times, thats how many more times I am going to tell you " + friends[x] +" that I am lucky to have you in my life");
+    p.appendChild(ptext);
+    div.appendChild(p);
     }
+        document.body.appendChild(div);
 }
+                            
+                            })
+
+})
